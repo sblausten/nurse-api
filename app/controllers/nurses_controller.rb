9239dd23b7249d8a828f4d38b11dd7324d7df822
Sam Blausten <sblausten@gmail.com>
2017-08-09 14:31:14 +0100

    Added endpoint for individual record retrieval

--- app/controllers/nurses_controller.rb
+++ app/controllers/nurses_controller.rb
@@ -1,12 +1,19 @@
 class NursesController < ApplicationController
 
   def index
-    begin
-      @nurses = Nurse.all
-      render json: @nurses, status: :ok
+    @nurses = Nurse.all
+    render json: @nurses, status: :ok
     rescue StandardError => e
       print e
-    end
+  end
+
+  def show
+    @nurse = Nurse.find(params[:id])
+    render json: @nurse, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: {
+        error: e.message
+      }, status: :not_found
   end
 
 end
--- spec/requests/nurses_spec.rb
+++ spec/requests/nurses_spec.rb
@@ -7,7 +7,7 @@ describe 'Nurses requests', type: :request do
   describe 'GET /nurses' do
 
     before :each {
-      get '/nurses'
+      get nurses_path
     }
 
     it 'returns status code 200' do
@@ -16,6 +16,38 @@ describe 'Nurses requests', type: :request do
 
     it 'returns all nurses in database' do
       expect(JSON.parse(response.body).size).to eq(10)
+    end
+  end
+
+  describe 'GET /nurses/:id' do
+
+    before {
+      get "/nurses/#{nurse_id}"
+    }
+
+    context 'when the record exists' do
+      let(:nurse_id) { nurses.last.id }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the nurse' do
+        expect(JSON.parse(response.body)['id']).to eq(nurse_id)
+      end
+    end
+
+    context 'when the record does not exist' do
+
+      let(:nurse_id) { 50 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Nurse with 'id'=/)
+      end
     end
   end
 end
